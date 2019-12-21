f048a2ae8bf292e3ebf4033bb7d0bbe083d01255
Julian Müller <julian.mueller@stud-mail.uni-wuerzburg.de>
2019-12-21 18:22:35 +0100

    variable speed, fade out particles on lifetime end, spawner object

--- public/javascripts/confetti.js
+++ public/javascripts/confetti.js
@@ -15,7 +15,7 @@ const confetti = function () {
     ctx.fillText("Merry Christmas :)", canvas.width / 2, canvas.height / 3);
     ////////////////////////////////// TODO remove tempt text test
 
-    const colors = [[165,104,246],[230,61,135],[0,199,228],[253,214,126]];
+    const colors = [[165, 104, 246], [230, 61, 135], [0, 199, 228], [253, 214, 126]];
 
     let running = false;
     const particles = [];
@@ -29,10 +29,8 @@ const confetti = function () {
         return array[random(array.length, true)];
     }
 
-    function makeColorString(colorArray = [0, 0, 0]) {
-        if (colorArray.length === 3) return "rgb(" + colorArray + ")";
-        if (colorArray.length === 4) return "rgba(" + colorArray + ")";
-        return "black";
+    function makeColorString(colorArray = [0, 0, 0], alpha = 1) {
+        return "rgba(" + colorArray + ", " + alpha + ")";
     }
 
     function spawnParticle(x = undefined, y = undefined) {
@@ -40,13 +38,17 @@ const confetti = function () {
             x: x !== undefined ? x : random(canvas.width),
             y: y !== undefined ? y : random(canvas.height),
             radius: random(4) + 1,
-            color: makeColorString(choose(colors)),
-            lifetime: random(200) + 50,
+            color: choose(colors),
+            lifetime: random(400) + 100,
+            speed: random(1, false) + 0.5,
         });
+        if (!running) requestAnimationFrame(animation);
+        running = true;
     }
 
     function drawParticle(particle) {
-        ctx.fillStyle = particle.color;
+        const opacity = particle.lifetime < 10 ? particle.lifetime / 10 : 1;
+        ctx.fillStyle = makeColorString(particle.color, opacity);
         ctx.beginPath();
         ctx.arc(particle.x, particle.y, particle.radius, 0, Math.PI * 2);
         ctx.fill();
@@ -54,26 +56,63 @@ const confetti = function () {
 
     function updateParticle(particle) {
         particle.lifetime -= 1;
-        particle.y += 1;
+        particle.y += particle.speed;
     }
 
     function removeDeadParticles(particles) {
-        for (let i = particles.length -1; i >= 0; i--) if (!particles[i].lifetime) particles.splice(i, 1);
+        for (let i = particles.length - 1; i >= 0; i--) if (!particles[i].lifetime) particles.splice(i, 1);
     }
 
     function animation() {
+        // draw
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
         particles.forEach(drawParticle);
+
+        //update
         particles.forEach(updateParticle);
         removeDeadParticles(particles);
         running = particles.length > 0;
+
+        //next frame
         if (running) requestAnimationFrame(animation);
+        else requestAnimationFrame(() => ctx.clearRect(0, 0, canvas.width, canvas.height));
     }
 
+    function topDelayed(delayEveryNParticles = 3) {
+        let counter = 0;
+        let x = random(canvas.width);
+        let y = 0;
 
-    function confetti(number) {
-        for (let i = 0; i < number; i++) spawnParticle();
-        if (!running) requestAnimationFrame(animation);
-        running = true;
+        function inc() {
+            counter++;
+            x = random(canvas.width);
+        }
+
+        return {
+            get x() {
+                return x;
+            },
+            get y() {
+                return y;
+            },
+            get waitForNextFrame() {
+                return counter % delayEveryNParticles === 0;
+            },
+            get next() {
+                return inc;
+            }
+        }
+    }
+
+    function confetti(number, spawn = topDelayed()) {
+        for (let i = 0; i < number; i++) {
+            spawn.next();
+            spawnParticle(spawn.x, spawn.y);
+            if (spawn.waitForNextFrame) {
+                requestAnimationFrame(() => confetti(number - i - 1, spawn));
+                break;
+            }
+        }
     }
 
     confetti.colors = colors;
